bac8e415f6821fad962dbebb88c19bb8157acf4f
Luke Brown <lukebrown14@hotmail.com>
2023-12-18 12:39:38 +0000

    Added Get function

--- main.go
+++ main.go
@@ -14,14 +14,50 @@ type CyptoPrice struct {
 	Price float32 `json:"rate"`
 }
 
+func apiGetRequest(nameInput string, currencyInput string) (string, string, float32) {
+
+	client := &http.Client {}
+	req, err := http.NewRequest("GET", "https://rest.coinapi.io/v1/exchangerate/"+ nameInput + "/" + currencyInput, nil)
+
+	checkError(err)
+
+	req.Header.Add("Accept", "text/plain")
+	req.Header.Add("X-CoinAPI-Key", "03F605A1-F710-41A8-8D5A-601C87BFD070")
+	
+	res, err := client.Do(req)
+
+	checkError(err)
+	
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	checkError(err)
+
+	var cypto CyptoPrice
+	err = json.Unmarshal(body, &cypto)
+
+	checkError(err)
+	
+	// return data 
+	name, currency, price := cypto.Name, cypto.Currency, cypto.Price
+
+	return name, currency, price
+}
+
+func checkError (err error){
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 func main() {
 
 	// Defining Variables 
 	cyptoInput := "BTC"
 	currencyInput := "GBP"
-	client := &http.Client {}
-
-
+	
 	// Handling Flags 
 	if len(os.Args) >= 2 {
 		cyptoInput = os.Args[1]
@@ -29,41 +65,7 @@ func main() {
 	}
 
 	// API Request
-	req, err := http.NewRequest("GET", "https://rest.coinapi.io/v1/exchangerate/"+ cyptoInput + "/" + currencyInput, nil)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	req.Header.Add("Accept", "text/plain")
-	req.Header.Add("X-CoinAPI-Key", "03F605A1-F710-41A8-8D5A-601C87BFD070")
-	res, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer res.Body.Close()
-
-	// Handling Data
-	body, err := io.ReadAll(res.Body)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var cypto CyptoPrice
-	err = json.Unmarshal(body, &cypto)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	name, currency, price := cypto.Name, cypto.Currency, cypto.Price
+	name, currency, price := apiGetRequest(cyptoInput, currencyInput)
 
 	// Outputting results to CLI 
 	fmt.Printf("%s: %s %.0f\n", name, currency, price)
